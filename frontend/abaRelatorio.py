8ee0e25f64fd99695b603636626cd21f95b5dab3
SmithFernandes <smith.fernandes1337@gmail.com>
2023-03-01 23:34:11 -0400

    create funcoes de atualizacoes da tabela

--- frontend/abaRelatorio.py
+++ frontend/abaRelatorio.py
@@ -22,34 +22,7 @@ def create_navigation(top_frame):
     om1 = tk.OptionMenu(top_frame, options, "HTML", "PHP", "MySQL", "Python")
     om1.grid(row=0, column=2)
 
-def create_table(center):
-    password = "root"
-    database = "project_rt"
-    db = BancoDados(host="localhost", user="root", password=password, database=database)
-    db.connect()
-    #client_db = Clientes_DB(db)
-    register_db = Registro_DB(db)
-    id_client = 1
-
-
-    # Inicia o Treeview com as seguintes colunas:
-    dataCols = ('NOME', 'DATA', 'HORA', 'PRIORIDADE')
-    tree = ttk.Treeview(center, columns=dataCols, show='headings')
-    tree.grid(row=0, column=0, sticky=tk.N + tk.S + tk.W + tk.E)
-
-    # Barras de rolagem
-    ysb = ttk.Scrollbar(center, orient=tk.VERTICAL, command=tree.yview)
-    xsb = ttk.Scrollbar(center, orient=tk.HORIZONTAL, command=tree.xview)
-    tree['yscroll'] = ysb.set
-    tree['xscroll'] = xsb.set
-    ysb.grid(row=0, column=1, sticky=tk.N + tk.S)
-    xsb.grid(row=1, column=0, sticky=tk.E + tk.W)
-
-    # Define o textos do cabeçalho (nome em maiúsculas)
-    for c in dataCols:
-        tree.heading(c, text=c.title())
-
-    # Dados:
+def atualizar_table(tree, register_db):
     data = [
         ("Argentina", "Buenos Aires", "ARS"),
         ("Australia", "Canberra", "AUD"),
@@ -68,16 +41,43 @@ def create_table(center):
         ("United States", "Washington, D.C.", "USD"),
     ]
 
-    data2 = register_db.allDateClient(id_client)
-    print(data2)
-
     #
     # Insere cada item dos dados
     for item in data:
         tree.insert('', 'end', values=item)
+
+def create_table(center, register_db):
+
+    # Inicia o Treeview com as seguintes colunas:
+    dataCols = ('NOME', 'DATA', 'HORA', 'PRIORIDADE')
+    tree = ttk.Treeview(center, columns=dataCols, show='headings')
+    tree.grid(row=0, column=0, sticky=tk.N + tk.S + tk.W + tk.E)
+
+    # Barras de rolagem
+    ysb = ttk.Scrollbar(center, orient=tk.VERTICAL, command=tree.yview)
+    xsb = ttk.Scrollbar(center, orient=tk.HORIZONTAL, command=tree.xview)
+    tree['yscroll'] = ysb.set
+    tree['xscroll'] = xsb.set
+    ysb.grid(row=0, column=1, sticky=tk.N + tk.S)
+    xsb.grid(row=1, column=0, sticky=tk.E + tk.W)
+
+    # Define o textos do cabeçalho (nome em maiúsculas)
+    for c in dataCols:
+        tree.heading(c, text=c.title())
+
+    atualizar_table(tree, register_db)
+
 def novaJanela():
     new_win = tk.Toplevel()
     new_win.title("Nova Janela")
+
+    password = "root"
+    database = "project_rt"
+    db = BancoDados(host="localhost", user="root", password=password, database=database)
+    db.connect()
+    # client_db = Clientes_DB(db)
+    register_db = Registro_DB(db)
+    id_client = 1
 
     # layout all of the main containers
     new_win.grid_rowconfigure(1, weight=1)
@@ -90,7 +90,7 @@ def novaJanela():
 
     center = tk.Frame(new_win, width=50, height=40, padx=3, pady=3)
     center.grid(row=1, sticky="nsew")
-    create_table(center)
+    create_table(center, register_db)
 
     button_ok = tk.Button(new_win, text="OK", command=new_win.destroy)
     button_ok.grid(row=2, column=0, pady=5, sticky=tk.E)
